8e765212b748f3d9524fdd75243c47677adb6a68
Alex Vidal <alex.vidal@datadoghq.com>
2025-07-08 11:42:52 -0500

    feat: cross-compile the binary and build multi-arch images (#11)

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,5 +5,40 @@ IFS=$'\n\t'
 # Print commands, but do not expand variables (avoid leaking secrets)
 set -o verbose
 
-go build -o ./dist/commit-headless -buildvcs=false .
-./dist/commit-headless version | awk '{print $3}' > ./dist/VERSION.txt
+PLATFORMS=(
+    "darwin-amd64"
+    "darwin-arm64"
+    "linux-amd64"
+    "linux-arm64"
+)
+
+# Create the 'dist' directory if it doesn't exist
+mkdir -p dist
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+    # Extract GOOS and GOARCH from the platform string
+    GOOS=${PLATFORM%%-*}
+    GOARCH=${PLATFORM##*-}
+
+    echo "Building for $GOOS/$GOARCH..."
+
+    # Build the executable
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o "dist/commit-headless-$GOOS-$GOARCH" -buildvcs=false .
+done
+
+echo "Build completed."
+
+# Figure out which one to execute based on system plat/arch
+OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+case "$ARCH" in
+    x86_64)
+        ARCH="amd64"
+        ;;
+    aarch64)
+        ARCH="arm64"
+        ;;
+esac
+
+"./dist/commit-headless-$OS_NAME-$ARCH" version | awk '{print $3}' > ./dist/VERSION.txt
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM scratch
 
-COPY commit-headless /commit-headless
+# Populated by buildx with the target platform and architecture
+ARG TARGETOS
+ARG TARGETARCH
+
+# The context has two copies of commit-headless: commit-headless-linux-amd64 and
+# commit-headless-linux-arm64
+# We only need to copy the architecture appropriate one in-place.
+COPY commit-headless-${TARGETOS}-${TARGETARCH} /commit-headless
 
 ENTRYPOINT ["/commit-headless"]
